9c1cc3fd87052949fb5e6c10c1fd24183f5f627e
shawnwun <shawnwun@gmail.com>
2012-06-20 01:46:22 +0800

    chessboard configure

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 
-python src/simulation.py TPMap_nodes.txt TPMap_edges.txt traffic/weekend_1800_01.txt route/Baseline.txt interval/Baseline.txt 10000
+#python src/simulation.py TPMap_nodes.txt TPMap_edges.txt traffic/weekend_1800_01.txt route/Baseline.txt interval/Baseline.txt 10000
 
+python src/simulation.py TPMap_nodes.txt TPMap_edges.txt traffic/weekend_1800_01.txt route/Chessboard.txt interval/Chessboard.txt 10000
 
--- src/getAllEdge.py
+++ src/getAllEdge.py
@@ -5,8 +5,10 @@ from util import *
 import matplotlib.pyplot as plt
 
 fname = sys.argv[1]
+edge = sys.argv[2]
+trff = sys.argv[3]
 
-g = constructMap(fname)
+g = constructMap(fname,edge,trff)
 
 for e in g.edges():
     print e[0],e[1],g.node[e[0]]['NAME'],g.node[e[1]]['NAME']
